db884ef70d4c9aaf6efb1de750636573e5e2b7d4
Darko Bozhinovski <dbozhinovski@gmail.com>
2017-10-31 00:25:38 +0100

    Migration to hugo (initial)

---
+++ content/posts/first.md
@@ -0,0 +1,8 @@
+---
+title: "First"
+date: 2017-08-21T16:26:33+02:00
+draft: true
+tags: [test]
+---
+
+## One two
---
+++ static/js/main.js
@@ -0,0 +1,4 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('bubbles', '/js/bubbles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
