9964b22b1a403c624a80e5bad1b8dc5f7182e816
Darko Bozhinovski <dbozhinovski@gmail.com>
2018-02-19 15:51:55 +0100

    Adds netlify ident to js

--- static/js/main.js
+++ static/js/main.js
@@ -2,3 +2,13 @@
 particlesJS.load('bubbles', '/js/bubbles.json', function() {
   console.log('callback - particles.js config loaded');
 });
+
+if (window.netlifyIdentity) {
+  window.netlifyIdentity.on("init", user => {
+    if (!user) {
+      window.netlifyIdentity.on("login", () => {
+        document.location.href = "/admin/";
+      });
+    }
+  });
+}
