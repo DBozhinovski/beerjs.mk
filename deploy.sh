f501fa16cdb458e5b530e84b0119247b028ae96f
Darko Bozhinovski <dbozhinovski@gmail.com>
2018-03-14 23:56:53 +0100

    Resets default page

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-USER=root
-HOST=beerjs.mk
-DIR=var/www/beerjs.mk   # might sometimes be empty!
-
-hugo && rsync -avz --delete public/ ${USER}@${HOST}:~/${DIR}
-
-exit 0
--- themes/beerjsmk/static/js/main.js
+++
@@ -1,4 +0,0 @@
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('bubbles', '/js/bubbles.json', function() {
-  console.log('callback - particles.js config loaded');
-});
